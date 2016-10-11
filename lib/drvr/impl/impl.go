cd8c85088093a80e3305de8427c523c40adecd26
Neil O'Toole <neilotoole@apache.org>
2016-10-11 14:10:34 -0600

    saving progress

--- lib/drvr/impl/impl.go
+++ lib/drvr/impl/impl.go
@@ -1,6 +1,7 @@
 package impl
 
 import (
+	_ "github.com/neilotoole/sq/lib/drvr/impl/csv"
 	_ "github.com/neilotoole/sq/lib/drvr/impl/mysql"
 	_ "github.com/neilotoole/sq/lib/drvr/impl/postgres"
 	_ "github.com/neilotoole/sq/lib/drvr/impl/sqlite3"
