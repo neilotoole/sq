bd045b6748df558d0d3ffd25609c9bd3a3ff983b
Todd Papaioannou <todd@edgeworx.io>
2020-08-07 01:59:23 -0700

    Add ascii title to output

--- cli/cmd_version.go
+++ cli/cmd_version.go
@@ -8,6 +8,12 @@ import (
 	"github.com/neilotoole/sq/cli/buildinfo"
 )
 
+const fontTitle = "   _________ _  \n" +
+	"  / ___/ __ `/	\n" +
+	" (__  ) /_/ /	\n" +
+	"/____/\\__, /	\n" +
+	"        /_/ 	\n"
+
 func newVersionCmd() (*cobra.Command, runFunc) {
 	cmd := &cobra.Command{
 		Use:   "version",
@@ -18,13 +24,17 @@ func newVersionCmd() (*cobra.Command, runFunc) {
 }
 
 func execVersion(rc *RunContext, cmd *cobra.Command, args []string) error {
+
 	version := buildinfo.Version
 
 	// If buildinfo.Version is not set (building without ldflags),
 	// then we set a dummy version.
 	if version == "" {
-		version = "0.0.0.dev"
+		version = "0.0.0.dev-tp"
 	}
+
+	// Print our ascii font title
+	rc.wrtr.fmt.Header.Fprintf(rc.Out, "%s", fontTitle)
 
 	rc.wrtr.fmt.Hilite.Fprintf(rc.Out, "sq %s", version)
 
