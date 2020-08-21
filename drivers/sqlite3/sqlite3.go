b3667d60cdf6dcbb889631122d029baffc2efb85
Neil O'Toole <neilotoole@apache.org>
2020-08-20 21:08:59 -0600

    implementing SQLDriver.AlterTableAddColumn (#65)

--- drivers/sqlite3/sqlite3.go
+++ drivers/sqlite3/sqlite3.go
@@ -249,6 +249,18 @@ func (d *driveri) CreateTable(ctx context.Context, db sqlz.DB, tblDef *sqlmodel.
 	return errz.Err(stmt.Close())
 }
 
+// AlterTableAddColumn implements driver.SQLDriver.
+func (d *driveri) AlterTableAddColumn(ctx context.Context, db *sql.DB, tbl string, col string, kind sqlz.Kind) error {
+	q := fmt.Sprintf("ALTER TABLE %q ADD COLUMN %q ", tbl, col) + DBTypeForKind(kind)
+
+	_, err := db.ExecContext(ctx, q)
+	if err != nil {
+		return errz.Wrapf(err, "alter table: failed to add column %q to table %q", col, tbl)
+	}
+
+	return nil
+}
+
 // PrepareInsertStmt implements driver.SQLDriver.
 func (d *driveri) PrepareInsertStmt(ctx context.Context, db sqlz.DB, destTbl string, destColNames []string, numRows int) (*driver.StmtExecer, error) {
 	destColsMeta, err := d.getTableRecordMeta(ctx, db, destTbl, destColNames)
@@ -354,11 +366,6 @@ func (d *driveri) TableColumnTypes(ctx context.Context, db sqlz.DB, tblName stri
 	}
 
 	return colTypes, nil
-}
-
-// AlterTableAddColumn implements driver.Driver.
-func (d *driveri) AlterTableAddColumn(ctx context.Context, db sqlz.DB, tbl string, col string, kind sqlz.Kind, ordinal int) error {
-	return errz.New("not implemented")
 }
 
 func (d *driveri) getTableRecordMeta(ctx context.Context, db sqlz.DB, tblName string, colNames []string) (sqlz.RecordMeta, error) {
