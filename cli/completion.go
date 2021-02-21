58b2e7254466a59d8559890f38cd3c6c5566964c
Neil O'Toole <neilotoole@apache.org>
2021-02-21 01:27:37 -0700

    tidy up after handle table refactor

--- cli/completion.go
+++ cli/completion.go
@@ -14,8 +14,8 @@ type completionFunc func(cmd *cobra.Command, args []string, toComplete string) (
 
 var (
 	_ completionFunc = completeHandle
-	_ completionFunc = new(handleTableCompleter).complete
 	_ completionFunc = completeDriverType
+	_ completionFunc = new(handleTableCompleter).complete
 )
 
 // completeHandle is a completionFunc that suggests handles.
@@ -47,142 +47,15 @@ func completeDriverType(cmd *cobra.Command, args []string, toComplete string) ([
 	return types, cobra.ShellCompDirectiveNoFileComp
 }
 
-// completeHandleOrTable is a completionFunc that suggests
-// a handle ("@sakila_sl3"), table (".actor"),
-// or handle.table ("@sakila_sl3.actor").
-func completeHandleOrTable(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	rc := RunContextFrom(cmd.Context())
-	ctx, cancelFn := context.WithTimeout(cmd.Context(), rc.Config.Defaults.ShellCompletionTimeout)
-	defer cancelFn()
-
-	handles := rc.Config.Sources.Handles()
-
-	if toComplete == "" {
-		// There's no input yet.
-		// Therefore we want to return a union of all handles
-		// plus the tables from the active source.
-		activeSrc := rc.Config.Sources.Active()
-		if activeSrc == nil {
-			rc.Log.Debug("Active source is nil")
-			return nil, cobra.ShellCompDirectiveError
-		}
-
-		tables, err := getTableNames(ctx, rc, activeSrc.Handle)
-		if err != nil {
-			rc.Log.Error(err)
-			return nil, cobra.ShellCompDirectiveError
-		}
-
-		suggestions := handles
-		for _, table := range tables {
-			suggestions = append(suggestions, "."+table)
-		}
-
-		return suggestions, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
-	}
-
-	// There's some input. We expect the input to be of the
-	// the form "@handle" or ".table". That is, the input should
-	// start with either '@' or '.'.
-	switch toComplete[0] {
-	default:
-		// User input was something other than '@' or '.'
-		return nil, cobra.ShellCompDirectiveError
-	case '@':
-		// We're dealing with a handle.
-
-		// We could be dealing with a just the handle ("@sakila_sl3")
-		// or a handle.table ("@sakila_sl3.actor").
-		if strings.ContainsRune(toComplete, '.') {
-			// It's a handle with at least a partial table,
-			// such as "@sakila_sl3.fil"
-			handle, partialTbl, err := source.ParseTableHandle(toComplete)
-			if err != nil {
-				rc.Log.Error(err)
-				return nil, cobra.ShellCompDirectiveError
-			}
-
-			tables, err := getTableNames(ctx, rc, handle)
-			if err != nil {
-				rc.Log.Error(err)
-				return nil, cobra.ShellCompDirectiveError
-			}
-
-			var suggestions []string
-			for _, table := range tables {
-				if strings.HasPrefix(table, partialTbl) {
-					suggestions = append(suggestions, handle+"."+table)
-				}
-			}
-
-			return suggestions, cobra.ShellCompDirectiveNoFileComp
-		}
-
-		// Else, we're dealing with a handle
-		var matchingHandles []string
-		for _, handle := range handles {
-			if strings.HasPrefix(handle, toComplete) {
-				matchingHandles = append(matchingHandles, handle)
-			}
-		}
-
-		switch len(matchingHandles) {
-		case 0:
-			return nil, cobra.ShellCompDirectiveNoFileComp
-		default:
-			return matchingHandles, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
-		case 1:
-			// Only one handle match, so we will present that complete
-			// handle, plus a suggestion for each of the tables
-			// for that handle
-		}
-
-		tables, err := getTableNames(ctx, rc, matchingHandles[0])
-		if err != nil {
-			rc.Log.Error(err)
-			return nil, cobra.ShellCompDirectiveError
-		}
-
-		suggestions := []string{matchingHandles[0]}
-		for _, table := range tables {
-			suggestions = append(suggestions, matchingHandles[0]+"."+table)
-		}
-
-		return suggestions, cobra.ShellCompDirectiveNoFileComp
-	case '.':
-		// We're dealing with a table, from the active source.
-		// Continues below
-	}
-
-	activeSrc := rc.Config.Sources.Active()
-	if activeSrc == nil {
-		rc.Log.Debug("Active source is nil")
-		return nil, cobra.ShellCompDirectiveError
-	}
-	tables, err := getTableNames(ctx, rc, activeSrc.Handle)
-	if err != nil {
-		rc.Log.Error(err)
-		return nil, cobra.ShellCompDirectiveError
-	}
-
-	var suggestions []string
-	for _, table := range tables {
-		if strings.HasPrefix(table, toComplete[1:]) {
-			suggestions = append(suggestions, "."+table)
-		}
-	}
-
-	return suggestions, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
-}
-
 // handleTableCompleter encapsulates completion of a handle
-// ("@sakila_sl3"), table (".actor"), or handle.table
+// ("@sakila_sl3"), table (".actor"), or @HANDLE.TABLE
 // ("@sakila_sl3.actor"). Its complete method is a completionFunc.
 type handleTableCompleter struct {
 	// onlySQL, when true, filters out non-SQL sources.
 	onlySQL bool
 }
 
+// complete is the completionFunc for handleTableCompleter.
 func (c *handleTableCompleter) complete(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	rc := RunContextFrom(cmd.Context())
 	if err := rc.init(); err != nil {
@@ -211,6 +84,9 @@ func (c *handleTableCompleter) complete(cmd *cobra.Command, args []string, toCom
 	}
 }
 
+// completeTableOnly returns suggestions given input beginning with
+// a period. Effectively this is completion for tables in the
+// active src.
 func (c *handleTableCompleter) completeTableOnly(ctx context.Context, rc *RunContext, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	activeSrc := rc.Config.Sources.Active()
 	if activeSrc == nil {
@@ -245,11 +121,13 @@ func (c *handleTableCompleter) completeTableOnly(ctx context.Context, rc *RunCon
 	return suggestions, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
 }
 
+// completeHandle returns suggestions given input beginning with
+// a '@'. The returned suggestions could be @HANDLE, or @HANDLE.TABLE.
 func (c *handleTableCompleter) completeHandle(ctx context.Context, rc *RunContext, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	// We're dealing with a handle.
 
 	// But we could be dealing with just the handle ("@sakila_sl3")
-	// or a handle.table ("@sakila_sl3.actor").
+	// or a @HANDLE.TABLE ("@sakila_sl3.actor").
 	if strings.ContainsRune(toComplete, '.') {
 		// It's a handle with a full handle and at least a
 		// partial table name, such as "@sakila_sl3.fil"
@@ -314,7 +192,7 @@ func (c *handleTableCompleter) completeHandle(ctx context.Context, rc *RunContex
 		return matchingHandles, cobra.ShellCompDirectiveNoFileComp | cobra.ShellCompDirectiveNoSpace
 	case 1:
 		// Only one handle match, so we will present that complete
-		// handle, plus a suggestion for each of the tables
+		// handle, plus a suggestion (@HANDLE.TABLE) for each of the tables
 		// for that handle
 	}
 
@@ -395,10 +273,6 @@ func handleIsSQL(rc *RunContext, handle string) (bool, error) {
 }
 
 func getTableNames(ctx context.Context, rc *RunContext, handle string) ([]string, error) {
-	if err := rc.init(); err != nil {
-		return nil, err
-	}
-
 	src, err := rc.Config.Sources.Get(handle)
 	if err != nil {
 		return nil, err
