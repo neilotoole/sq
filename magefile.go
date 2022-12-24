2cc2a5e7a5484ed703ed15df11b0398115ceefea
neilotoole <neilotoole@apache.org>
2022-12-24 00:17:31 -0700

    fixed broken magefile

--- magefile.go
+++ magefile.go
@@ -8,6 +8,7 @@ package main
 // This file originally derived from the Hugo magefile, see https://github.com/gohugoio/hugo
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"time"
@@ -105,4 +106,83 @@ func CheckDocker() error {
 func execDocker(cmd string, args ...string) error {
 	args = append([]string{cmd}, args...)
 	return sh.RunV("docker", args...)
+}
+
+func gitHeadCommit(short bool) string {
+	args := []string{"rev-parse", "HEAD"}
+	if short {
+		args = []string{"rev-parse", "--short", "HEAD"}
+	}
+
+	hash, err := sh.Output("git", args...)
+	panicIf(err)
+	return hash
+}
+
+func gitCommitForLatestTag() string {
+	hash, err := sh.Output("git", "rev-list", "--tags", "--max-count=1")
+	panicIf(err)
+	return hash
+}
+
+func gitTagForCommit(commit string) string {
+	tag, err := sh.Output("git", "describe", "--tags", commit)
+	panicIf(err)
+	return tag
+}
+
+func gitLatestTag() string {
+	commitForLatestTag := gitCommitForLatestTag()
+	tag := gitTagForCommit(commitForLatestTag)
+	return tag
+}
+
+func gitCurrentBranch() string {
+	branch, err := sh.Output("git", "rev-parse", "--abbrev-ref", "HEAD")
+	panicIf(err)
+	return branch
+}
+
+func gitIsDirtyWorkingDir() bool {
+	diff, err := sh.Output("git", "diff", "HEAD")
+	panicIf(err)
+
+	// If no diff, then diff's output will be empty.
+	return diff != ""
+}
+
+// BuildVersion prints the build version that would be
+// incorporated into the sq binary. The build version is of
+// the form TAG[-SUFFIX], for example "v0.5.9-dev".
+//   - If working dir is dirty, or if the HEAD commit does not
+//     match the latest tag commit, the suffix is "-wip" (Work In
+//     Progress), e.g. "v0.5.9-wip".
+//   - Else, if the branch is not master, the branch name is
+//     used as the suffix, e.g. "v0.5.9-dev".
+//   - Else, we're on master and the HEAD commit is the latest
+//     tag, so the suffix is omitted, e.g. "v0.5.9".
+func BuildVersion() {
+	fmt.Println(generateBuildVersion())
+}
+
+func generateBuildVersion() string {
+	commitForLatestTag := gitCommitForLatestTag()
+	headCommit := gitHeadCommit(false)
+	latestTag := gitTagForCommit(commitForLatestTag)
+	currentBranch := gitCurrentBranch()
+
+	// If working dis is dirty or we're not on the latest tag,
+	// then add a "-wip" suffix (Work In Progress).
+	isDirty := gitIsDirtyWorkingDir()
+	if isDirty || headCommit != commitForLatestTag {
+		return latestTag + "-wip"
+	}
+
+	// Else, if the current branch is not master, append the
+	// branch.
+	if currentBranch != "master" {
+		return latestTag + "-" + currentBranch
+	}
+
+	return latestTag
 }
