ab5c78118ac517ae0f38b1ba0c367d11fe1007b9
neilotoole <neilotoole@apache.org>
2023-03-19 09:46:50 -0600

    sakila: yet more test data setup

--- drivers/sqlite3/testdata/sakila-whitespace-alter.sql
+++ drivers/sqlite3/testdata/sakila-whitespace-alter.sql
@@ -1,5 +1,5 @@
 ALTER TABLE actor RENAME COLUMN "first_name" TO "first name";
 
-ALTER TABLE actor RENAME COLUMN "last_name" TO "last_name";
+ALTER TABLE actor RENAME COLUMN "last_name" TO "last name";
 
 ALTER TABLE "film_actor" RENAME TO "film actor";
