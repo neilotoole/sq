b899cf2bc830383a8b17fc4e6094df992c1a0571
neilotoole <neilotoole@apache.org>
2023-03-25 20:47:29 -0600

    workflow: fiddling with custom release notes

--- .generate-changelog-delta.sh
+++ .generate-changelog-delta.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
 # This script the per-release changelog file for goreleaser to use
-# to publish to GitHub releases.
+# to publish to GitHub releases. It produces markdown output.
+
+# First we get the current and previous git tags.
 curTag=$(git tag --sort=-creatordate | head -n 1)
 prevTag=$(git tag --sort=-creatordate | head -n 2 | tail -n 1)
-git diff "$prevTag" "$curTag" --no-ext-diff --unified=0 --exit-code -a --no-prefix -- ./CHANGELOG.md \
-| grep -E "^\+" | grep -v '+++ CHANGELOG.md' | cut  -c 2-
 
-echo '### Commits'
+# Then we run git diff on CHANGELOG.md;
+# and grep for only the added lines;
+# and stripping out the '+++ CHANGELOG.md' header line;
+# and stripping the leading '+' from each line
+# and getting rid of a superfluous starting newline.
+git diff "$prevTag" "$curTag" --no-ext-diff --unified=0 --exit-code -a --no-prefix -- ./CHANGELOG.md \
+| grep -E "^\+" \
+| grep -vF '## [v' \
+| grep -vF '+++ CHANGELOG.md' \
+| cut -c 2- \
+| tail -n +2
+
+
+
+# Then we add a section for the commits.
+printf "\n### Commits\n\n"
 
 echo '```text'
 git log --pretty=format:'%h   %s%n' "$prevTag".."$curTag"
