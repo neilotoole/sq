256d9c1ab66e8214ef344620be9ec07e8499f1c0
Neil O'Toole <neilotoole@apache.org>
2023-04-08 06:39:06 -0600

    go.mod:  upgrade to jackc/pgx v5 (#184)
    * jackc/pgx upgrade
    
    * jackc/pgx upgrade

--- drivers/postgres/postgres_test.go
+++ drivers/postgres/postgres_test.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/neilotoole/sq/libsq/source"
 
-	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
