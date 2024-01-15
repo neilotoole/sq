9a6245b62d465be00c08166a0379e50ba9ea74e7
neilotoole <neilotoole@apache.org>
2024-01-14 20:20:17 -0700

    Added progress bar to libsq.QuerySQL

--- libsq/libsq.go
+++ libsq/libsq.go
@@ -11,11 +11,11 @@ package libsq
 
 import (
 	"context"
-
 	"github.com/neilotoole/sq/libsq/core/errz"
 	"github.com/neilotoole/sq/libsq/core/lg"
 	"github.com/neilotoole/sq/libsq/core/lg/lga"
 	"github.com/neilotoole/sq/libsq/core/lg/lgm"
+	"github.com/neilotoole/sq/libsq/core/progress"
 	"github.com/neilotoole/sq/libsq/core/record"
 	"github.com/neilotoole/sq/libsq/core/sqlz"
 	"github.com/neilotoole/sq/libsq/driver"
@@ -139,7 +139,9 @@ func QuerySQL(ctx context.Context, grip driver.Grip, db sqlz.DB, //nolint:funlen
 		}
 	}
 
+	bar := progress.FromContext(ctx).NewWaiter("Execute query", true)
 	rows, err := db.QueryContext(ctx, query, args...)
+	bar.Stop()
 	if err != nil {
 		err = errz.Wrapf(errw(err), `SQL query against %s failed: %s`, grip.Source().Handle, query)
 		select {
