0fd8e3da7854c070c9be1019db033f650686cdff
neilotoole <neilotoole@apache.org>
2024-02-12 12:56:27 -0700

    wip

--- libsq/core/tailbuf/tailbuf.go
+++ libsq/core/tailbuf/tailbuf.go
@@ -3,11 +3,18 @@
 // [tailbuf.New] to create a [Buf].
 package tailbuf
 
-import "fmt"
+import "context"
 
-// Buf is an append-only circular buffer of fixed size that provides a window on
-// the tail of the items written to the buffer. The zero value is not usable;
-// invoke [tailbuf.New] to create a [Buf]. It is not safe for concurrent use.
+// Buf is an append-only fixed-size circular buffer that provides a window on
+// the tail of items written to the buffer. The zero value is not usable; invoke
+// [tailbuf.New] to create a [Buf]. It is not safe for concurrent use.
+//
+// Note the terms "nominal buffer" and "tail window" (or just "window"). The
+// nominal buffer is the complete list of items written to Buf via the Buf.Write
+// or Buf.WriteAll methods. However, Buf drops the oldest items as it fills
+// (which is the entire point of this package): the tail window is the subset of
+// the nominal buffer that is currently available. Some of Buf's methods take
+// arguments that are indices into the nominal buffer, for example [Buf.Slice].
 type Buf[T any] struct {
 	// back is the cursor for the oldest item.
 	back int
@@ -87,10 +94,18 @@ func (b *Buf[T]) Count() int {
 	return b.count
 }
 
-// Range returns the start and end indices of the current window vs the nominal
+func (b *Buf[T]) InBounds(i int) bool {
+	if b.count == 0 {
+		return false
+	}
+	start, end := b.Bounds()
+	return i >= start && i <= end // TODO: should be < end?
+}
+
+// Bounds returns the start and end indices of the current window vs the nominal
 // complete list of items written to the buffer. If the buffer is empty, start
 // and end are both 0. The returned end value always equals [Buf.Count].
-func (b *Buf[T]) Range() (start, end int) {
+func (b *Buf[T]) Bounds() (start, end int) {
 	if b.front == -1 {
 		return 0, 0
 	}
@@ -106,8 +121,27 @@ func (b *Buf[T]) Range() (start, end int) {
 	return start, end
 }
 
-func (b *Buf[T]) Slice(start, end int) []T {
+// Slice returns a slice into the nominal buffer, using the standard
+// [inclusive:exclusive] slicing mechanics. Slice panics if start is negative or
+// end is less than start.
+//
+// Boundary checking is relaxed. If the buffer is empty, the returned slice
+// is empty. Otherwise, if the requested range is completely outside the bounds
+// of the tail window, the returned slice is empty; if the range overlaps with
+// the tail window, the returned slice contains the overlapping items. If strict
+// boundary checking is important to you, use [Buf.InBounds] to check the start
+// and end indices.
+//
+// Slice is approximately functionality equivalent to reslicing the result of
+// [Buf.Window], but it avoids wasteful copying (and has relaxed boundarcy
+// checking).
+//
+//  buf := tailbuf.New[int](3).WriteAll(1, 2, 3)
+//  a := buf.Window()[0:2]
+//  b := buf.Slice(0, 2)
+//  assert.Equal(t, a, b)
 
+func (b *Buf[T]) Slice(start, end int) []T {
 	switch {
 	case start < 0:
 		panic("start must be >= 0")
@@ -123,19 +157,24 @@ func (b *Buf[T]) Slice(start, end int) []T {
 		return make([]T, 0)
 	case b.front > b.back:
 		offset := b.count - len(b.window)
-		//backo := start - offset
-		//fronto := end - offset
-		//s := b.window[backo:fronto]
-		//backo :=
-		//fronto :=
 		s := b.window[start-offset : end-offset]
 		return s
+	case b.count == 1:
+		// Special case: the buffer has only one item.
+		if start == 0 && end > 1 {
+			return []T{b.window[0]}
+		}
+		return make([]T, 0)
+
 	default: // b.back > b.front
-		offset := b.count - len(b.window)
+		var offset int
+		if b.count > len(b.window) {
+			offset = b.count - len(b.window)
+		}
 		backo := start - offset
-		fronto := end - offset
-		fmt.Printf("found it! back:%d, front: %d, start: %d, end: %d, backo: %d, fronto: %d\n",
-			b.back, b.front, start, end, backo, fronto)
+		//fronto := end - offset
+		//fmt.Printf("found it! back:%d, front: %d, start: %d, end: %d, backo: %d, fronto: %d\n",
+		//	b.back, b.front, start, end, backo, fronto)
 
 		back := b.window[b.back+backo:]
 		front := b.window[:b.front]
@@ -146,6 +185,17 @@ func (b *Buf[T]) Slice(start, end int) []T {
 		//panic()
 		//return append(b.window[backo:], b.window[:fronto+1]...)
 	}
+}
+
+// Offset returns the offset of the current window vs the nominal complete list
+// of items written to the buffer. If the buffer is empty, the returned offset
+// is 0.
+func (b *Buf[T]) Offset() int {
+	if b.count <= len(b.window) {
+		return 0
+	}
+
+	return b.count - len(b.window)
 }
 
 func (b *Buf[T]) AtOffset(offset int) T {
@@ -164,10 +214,108 @@ func (b *Buf[T]) AtOffset(offset int) T {
 }
 
 func (b *Buf[T]) Front() T {
+	if b.front == -1 {
+		var t T
+		return t
+	}
+	return b.window[b.front]
+}
+
+func (b *Buf[T]) Back() T {
 	if b.back == -1 {
 		var t T
 		return t
 	}
+	return b.window[b.back]
+}
 
-	return b.window[b.front]
+// Apply applies fn to each item in the tail window, in oldest-to-newest order.
+// If the buffer is empty, fn is not invoked. The buffer is returned for
+// chaining. Example:
+//
+//	buf := tailbuf.New[string](3)
+//	buf.WriteAll("a", "b  ", "   c  ")
+//	buf.Apply(strings.ToUpper).Apply(strings.TrimSpace)
+//	fmt.Println(buf.Window())
+//	// Output: [A B C]
+//
+// Using Apply is cheaper than getting the window and applying the function
+// manually, as it avoids the allocation of a new slice by Buf.Window.
+//
+// For more control or to handle errors, use [Buf.Do].
+func (b *Buf[T]) Apply(fn func(item T) T) *Buf[T] {
+	if b.count == 0 {
+		return b
+	}
+
+	if b.front > b.back {
+		for i := b.back; i <= b.front; i++ {
+			b.window[i] = fn(b.window[i])
+		}
+		return b
+	}
+
+	for i := b.back; i < len(b.window); i++ {
+		b.window[i] = fn(b.window[i])
+	}
+
+	for i := 0; i <= b.front; i++ {
+		b.window[i] = fn(b.window[i])
+	}
+	return b
+}
+
+// Do applies fn to each item in the tail window, in oldest-to-newest order,
+// replacing each item with the value returned by successful invocation of fn.
+// If fn returns an error, the item is not replaced. Execution is halted if any
+// invocation of fn returns an error, and that error is returned to the caller.
+// Thus a partial application of fn may occur.
+//
+// If Buf is empty, fn is not invoked.
+//
+// The index arg to fn is the index of the item in the tail window. You can use
+// the offset arg to compute the index of the item in the nominal buffer.
+//
+//	nominalIndex := index + offset
+//
+// REVISIT: Should index be the tailIndex instead?
+//
+// The context is not checked for cancellation between iterations. The context
+// should be checked in fn if desired.
+func (b *Buf[T]) Do(ctx context.Context, fn func(ctx context.Context, item T, index, offset int) (T, error)) error {
+	if b.count == 0 {
+		return nil
+	}
+
+	var v T
+	var err error
+
+	if b.front > b.back {
+		for i := b.back; i <= b.front; i++ {
+			v, err = fn(ctx, b.window[i], i, i-b.back)
+			if err != nil {
+				return err
+			}
+			b.window[i] = v
+		}
+		return nil
+	}
+
+	for i := b.back; i < len(b.window); i++ {
+		v, err = fn(ctx, b.window[i], i, i-b.back)
+		if err != nil {
+			return err
+		}
+		b.window[i] = v
+	}
+
+	for i := 0; i <= b.front; i++ {
+		v, err = fn(ctx, b.window[i], i, i-b.back)
+		if err != nil {
+			return err
+		}
+		b.window[i] = v
+	}
+
+	return nil
 }
--- libsq/core/tailbuf/tailbuf_test.go
+++ libsq/core/tailbuf/tailbuf_test.go
@@ -1,26 +1,28 @@
 package tailbuf_test
 
 import (
+	"fmt"
 	"github.com/neilotoole/sq/libsq/core/tailbuf"
 	"github.com/neilotoole/sq/testh/tu"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
 func TestBuf(t *testing.T) {
 	testCases := []struct {
-		add        string
-		start, end int
-		window     []string
+		add                string
+		wantStart, wantEnd int
+		wantWindow         []string
 	}{
-		{add: "a", start: 0, end: 1, window: []string{"a"}},
-		{add: "b", start: 0, end: 2, window: []string{"a", "b"}},
-		{add: "c", start: 0, end: 3, window: []string{"a", "b", "c"}},
-		{add: "d", start: 1, end: 4, window: []string{"b", "c", "d"}},
-		{add: "e", start: 2, end: 5, window: []string{"c", "d", "e"}},
-		{add: "f", start: 3, end: 6, window: []string{"d", "e", "f"}},
-		{add: "g", start: 4, end: 7, window: []string{"e", "f", "g"}},
-		{add: "h", start: 5, end: 8, window: []string{"f", "g", "h"}},
+		{add: "a", wantStart: 0, wantEnd: 1, wantWindow: []string{"a"}},
+		//{add: "b", start: 0, end: 2, window: []string{"a", "b"}},
+		//{add: "c", start: 0, end: 3, window: []string{"a", "b", "c"}},
+		//{add: "d", start: 1, end: 4, window: []string{"b", "c", "d"}},
+		//{add: "e", start: 2, end: 5, window: []string{"c", "d", "e"}},
+		//{add: "f", start: 3, end: 6, window: []string{"d", "e", "f"}},
+		//{add: "g", start: 4, end: 7, window: []string{"e", "f", "g"}},
+		//{add: "h", start: 5, end: 8, window: []string{"f", "g", "h"}},
 	}
 
 	buf := tailbuf.New[string](3)
@@ -29,24 +31,41 @@ func TestBuf(t *testing.T) {
 		tc := tc
 		t.Run(tu.Name(i, tc.add), func(t *testing.T) {
 			buf.Write(tc.add)
-			require.Equal(t, tc.end, buf.Count())
+			require.Equal(t, tc.wantEnd, buf.Count())
 			require.Equal(t, tc.add, buf.Front())
 			window := buf.Window()
-			require.Equal(t, tc.window, window)
-			start, end := buf.Range()
-			require.Equal(t, tc.start, start)
-			require.Equal(t, tc.end, end)
-			s := buf.Slice(start, end)
+			require.Equal(t, tc.wantWindow, window)
+			start, end := buf.Bounds()
+			require.Equal(t, tc.wantStart, start)
+			require.Equal(t, tc.wantEnd, end)
+			s := buf.Slice(start, end+1)
 			require.Equal(t, window, s)
 		})
 	}
+}
+
+func TestBuf_Slice2(t *testing.T) {
+	buf := tailbuf.New[string](3)
+	buf.Write("a")
+	window := buf.Window()
+	require.Equal(t, []string{"a"}, window)
+	start, end := buf.Bounds()
+	require.Equal(t, 0, start)
+	require.Equal(t, 1, end)
+	s := buf.Slice(0, 0)
+	require.Empty(t, s)
+	s = buf.Slice(0, 1)
+	require.Empty(t, s)
+	s = buf.Slice(0, 2)
+	require.Equal(t, []string{"a"}, s)
+
 }
 
 func TestBuf_Slice(t *testing.T) {
 	buf := tailbuf.New[int](3)
 	buf.WriteAll(0, 1, 2)
 
-	start, end := buf.Range()
+	start, end := buf.Bounds()
 	require.Equal(t, 0, start)
 	require.Equal(t, 3, end)
 	s := buf.Slice(start, end)
@@ -68,7 +87,7 @@ func TestBuf_Slice(t *testing.T) {
 	require.Equal(t, []int{1, 2}, s)
 
 	buf.WriteAll(3, 4, 5)
-	start, end = buf.Range()
+	start, end = buf.Bounds()
 	require.Equal(t, 3, start)
 	require.Equal(t, 6, end)
 	s = buf.Slice(start, end)
@@ -84,4 +103,28 @@ func TestBuf_Slice(t *testing.T) {
 	buf.WriteAll(6, 7)
 	s = buf.Slice(6, 7)
 	require.Equal(t, []int{6}, s)
+}
+
+func TestBuf_Apply(t *testing.T) {
+	buf := tailbuf.New[string](3)
+	buf.WriteAll("In", "Xanadu  ", "   did", "Kubla  ", "Khan")
+	buf.Apply(strings.ToUpper).Apply(strings.TrimSpace)
+	got := buf.Window()
+	require.Equal(t, []string{"DID", "KUBLA", "KHAN"}, got)
+}
+
+func TestBuf_Apply2(t *testing.T) {
+	buf := tailbuf.New[string](3)
+	buf.WriteAll("a", "b  ", "   c  ")
+	buf.Apply(strings.ToUpper).Apply(strings.TrimSpace)
+	fmt.Println(buf.Window())
+	//t.Log(got)
+	//require.Equal(t, []string{"A", "B", "C"}, got)
+}
+
+func Test_Window_Slice_Equivalence(t *testing.T) {
+	buf := tailbuf.New[int](3).WriteAll(1, 2, 3, 4, 5)
+	a := buf.Window()[0:2]
+	b := buf.Slice(0, 2)
+	require.Equal(t, a, b)
 }
