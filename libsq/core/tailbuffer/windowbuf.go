8803640e95fe9e005da09e7b695f0709cc271f62
neilotoole <neilotoole@apache.org>
2024-02-12 08:02:04 -0700

    tailbuffer: partially working

--- libsq/core/tailbuffer/windowbuf.go
+++ libsq/core/tailbuffer/windowbuf.go
@@ -8,25 +8,40 @@ func Huzzah() {
 
 type Buffer[T any] struct {
 	window []T
-	next   int
+	front  int
 	back   int
 	count  int
 }
 
 func New[T any](size int) *Buffer[T] {
-	return &Buffer[T]{window: make([]T, size), next: -1, back: -1}
+	return &Buffer[T]{
+		window: make([]T, size),
+		back:   -1,
+		front:  -1,
+	}
 }
 
-func (b *Buffer[T]) Append(item T) *Buffer[T] {
+func (b *Buffer[T]) Write(item T) *Buffer[T] {
 	b.count++
-	if b.next == -1 {
-		b.next = 0
+	switch {
+	case b.front == -1:
+		b.back = 0
+	case b.count < len(b.window):
+	default:
+		b.back = (b.back + 1) % len(b.window)
 	}
-	b.back = (b.back + 1) % len(b.window)
-	b.window[b.back] = item
-	if b.back == b.next {
-		b.next = (b.next + 1) % len(b.window)
-	}
+
+	//if b.front == -1 {
+	//	b.back = 0
+	//} else if b.front == len(b.window) {
+	//	b.back = (b.back + 1) % len(b.window)
+	//}
+
+	b.front = (b.front + 1) % len(b.window)
+	b.window[b.front] = item
+	//if b.front == b.back {
+	//	b.back = (b.back + 1) % len(b.window)
+	//}
 	return b
 }
 
@@ -38,29 +53,37 @@ func (b *Buffer[T]) Window() []T {
 		return b.window[0:b.count]
 	}
 
-	if b.back >= b.next {
-		return b.window[b.next : b.back+1]
+	if b.front >= b.back {
+		return b.window[b.back : b.front+1]
 	}
-	return append(b.window[b.next:], b.window[:b.back+1]...)
+	return append(b.window[b.back:], b.window[:b.front+1]...)
 }
 
 func (b *Buffer[T]) Range() (start, end int) {
-	if b.next == -1 {
+	if b.front == -1 {
 		return 0, 0
 	}
 
-	return b.back, b.next
+	end = b.count
+	if b.count <= len(b.window) {
+		start = 0
+
+	} else {
+		start = b.count - len(b.window)
+	}
+
+	return start, end
 }
 
 func (b *Buffer[T]) AtOffset(offset int) T {
-	if b.next == -1 {
+	if b.back == -1 {
 		var t T
 		return t
 	}
 
 	x := offset - b.count - len(b.window)
 
-	i := (b.back + offset) % len(b.window)
+	i := (b.front + offset) % len(b.window)
 	i = i - b.count
 
 	return b.window[x]
@@ -68,10 +91,10 @@ func (b *Buffer[T]) AtOffset(offset int) T {
 }
 
 func (b *Buffer[T]) Front() T {
-	if b.next == -1 {
+	if b.back == -1 {
 		var t T
 		return t
 	}
 
-	return b.window[b.back]
+	return b.window[b.front]
 }
--- libsq/core/tailbuffer/windowbuf_test.go
+++ libsq/core/tailbuffer/windowbuf_test.go
@@ -8,16 +8,16 @@ import (
 
 func TestBuffer(t *testing.T) {
 	buf := tailbuffer.New[int](3)
-	buf.Append(0).Append(1).Append(2)
+	buf.Write(0).Write(1).Write(2)
 	require.Equal(t, []int{0, 1, 2}, buf.Window())
 	require.Equal(t, 2, buf.Front())
 
 	a := buf.Window()
 	t.Log(a)
 
-	buf.Append(3)
-	buf.Append(4)
-	buf.Append(5)
+	buf.Write(3)
+	buf.Write(4)
+	buf.Write(5)
 	a = buf.Window()
 	t.Log(a)
 }
@@ -32,22 +32,33 @@ func TestRange(t *testing.T) {
 	require.Equal(t, 0, end)
 
 	// Add an item
-	buf.Append("a")
+	buf.Write("a")
 	window = buf.Window()
 	require.Equal(t, []string{"a"}, window)
 	start, end = buf.Range()
-	t.Log(start, end)
+	//r := window[start:end]
+	//t.Log(start, end, r)
 	require.Equal(t, 0, start)
 	require.Equal(t, 1, end)
 
-	// Add two more items
-	buf.Append("b")
-	buf.Append("c")
+	// Add another item
+	buf.Write("b")
+	window = buf.Window()
+	require.Equal(t, []string{"a", "b"}, window)
+	start, end = buf.Range()
+	//r = window[start:end]
+
+	//t.Log(start, end, r)
+	require.Equal(t, 0, start)
+	require.Equal(t, 2, end)
+
+	buf.Write("c")
 	window = buf.Window()
 	require.Equal(t, []string{"a", "b", "c"}, window)
 	start, end = buf.Range()
-	t.Log(start, end)
+	//r = window[start:end]
+	//t.Log(start, end, r)
 	require.Equal(t, 0, start)
-	require.Equal(t, 2, end)
+	require.Equal(t, 3, end)
 
 }
