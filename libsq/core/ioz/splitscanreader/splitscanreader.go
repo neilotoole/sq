1d2f6f12a29cd02fe254e535992ad539862580cc
neilotoole <neilotoole@apache.org>
2024-11-29 13:56:32 -0700

    Mostly working

---
+++ libsq/core/ioz/splitscanreader/splitscanreader.go
@@ -0,0 +1,96 @@
+package splitscanreader
+
+import (
+	"bytes"
+	"errors"
+	"io"
+)
+
+/*
+
+https://stackoverflow.com/questions/37530451/golang-bufio-read-multiline-until-crlf-r-n-delimiter/37531472
+*/
+
+type Scanner struct {
+	src  io.Reader
+	buf  *bytes.Buffer
+	done bool
+}
+
+func NewScanner(src io.Reader) *Scanner {
+	return &Scanner{src: src, buf: &bytes.Buffer{}}
+}
+
+func (s *Scanner) Scan() bool {
+	return !s.done
+}
+
+// Reader returns the next reader, or nil.
+func (s *Scanner) Reader() io.Reader {
+	if s.done {
+		return nil
+	}
+	return &reader{sc: s}
+}
+
+var _ io.Reader = &reader{}
+
+type reader struct {
+	sc   *Scanner
+	done bool
+}
+
+func (r reader) Read(p []byte) (n int, err error) {
+	if r.done {
+		return 0, io.EOF
+	}
+
+	if r.sc.buf.Len() > 0 {
+		n, err = r.sc.buf.Read(p)
+	} else {
+		n, err = r.sc.src.Read(p)
+	}
+
+	if err != nil {
+		r.sc.done = true
+
+		if n == 0 && errors.Is(err, io.EOF) {
+			return 0, io.EOF
+		}
+
+		return n, err
+	}
+
+	data := p[:n]
+	i := bytes.IndexByte(data, '\n')
+
+	if i < 0 {
+		i = bytes.IndexByte(data, '\r')
+		if i < 0 {
+			return n, nil
+		}
+	}
+
+	if i == 0 {
+		if n == 1 {
+			return 0, io.EOF
+		}
+
+		_, err = r.sc.buf.Write(p[i+1 : n])
+		if err != nil {
+			return i, err
+		}
+
+		return 0, io.EOF
+	}
+
+	_, err = r.sc.buf.Write(p[i+1 : n])
+	if p[i-1] == '\r' {
+		i--
+	}
+	if err != nil {
+		return i, err
+	}
+
+	return i, io.EOF
+}
---
+++ libsq/core/ioz/splitscanreader/splitscanreader_test.go
@@ -0,0 +1,171 @@
+package splitscanreader_test
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/neilotoole/sq/libsq/core/ioz/splitscanreader"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var testCases = []struct {
+	name         string
+	in           string
+	wantLines    []string
+	wantRdrCount int
+}{
+	{
+		name:         "empty",
+		in:           "",
+		wantLines:    nil,
+		wantRdrCount: 1,
+	},
+	{
+		name:         "oneline-lf",
+		in:           "a\n",
+		wantLines:    []string{"a"},
+		wantRdrCount: 2,
+	},
+	{
+		name:         "empty-1-lf",
+		in:           "\n",
+		wantLines:    nil,
+		wantRdrCount: 2,
+	},
+	{
+		name:         "empty-1-crlf",
+		in:           "\r\n",
+		wantLines:    nil,
+		wantRdrCount: 2,
+	},
+	{
+		name:         "empty-2-crlf",
+		in:           "\r\n\r\n",
+		wantLines:    nil,
+		wantRdrCount: 3,
+	},
+	{
+		name:         "empty-2-lf",
+		in:           "\n\n",
+		wantLines:    nil,
+		wantRdrCount: 3,
+	},
+	{
+		name:         "oneline-crlf",
+		in:           "line1\r\n",
+		wantLines:    []string{"line1"},
+		wantRdrCount: 2,
+	},
+	{
+		name:         "oneline-no-lf",
+		in:           "line1",
+		wantLines:    []string{"line1"},
+		wantRdrCount: 1,
+	},
+	{
+		name:         "content-2-lf",
+		in:           "line1\nline2\n",
+		wantLines:    []string{"line1", "line2"},
+		wantRdrCount: 3,
+	},
+	{
+		name:         "content-4-no-trailing-lf",
+		in:           "line1\nline2\nline3\nline4",
+		wantLines:    []string{"line1", "line2", "line3", "line4"},
+		wantRdrCount: 4,
+	},
+	{
+		name:         "single-char-4-lf",
+		in:           "a\nb\nc\nd",
+		wantLines:    []string{"a", "b", "c", "d"},
+		wantRdrCount: 4,
+	},
+	{
+		name:         "single-char-4-cr",
+		in:           "a\rb\rc\rd",
+		wantLines:    []string{"a", "b", "c", "d"},
+		wantRdrCount: 4,
+	},
+	{
+		name:         "multi-lines-with-extra-lf",
+		in:           "\nline2\nline3\nline4\n\nline5",
+		wantLines:    []string{"line2", "line3", "line4", "line5"},
+		wantRdrCount: 6,
+	},
+}
+
+func Test_ReadAll(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rdrCount := 0
+
+			sc := splitscanreader.NewScanner(strings.NewReader(tc.in))
+			var lines []string
+			for sc.Scan() {
+				r := sc.Reader()
+				require.NotNil(t, r)
+				rdrCount++
+
+				line, err := io.ReadAll(r)
+				assert.NoError(t, err)
+
+				if len(line) > 0 {
+					lines = append(lines, string(line))
+				}
+			}
+			require.Equal(t, tc.wantLines, lines)
+			require.Equal(t, tc.wantRdrCount, rdrCount)
+		})
+	}
+}
+
+func Test_Read_Loop(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for bufSize := 1; bufSize < 10; bufSize++ { // FIXME: test bufsize zero
+				t.Run(fmt.Sprintf("buf-%d", bufSize), func(t *testing.T) {
+					rdrCount := 0
+					sc := splitscanreader.NewScanner(strings.NewReader(tc.in))
+					var lines []string
+
+					for sc.Scan() {
+						r := sc.Reader()
+						require.NotNil(t, r)
+						rdrCount++
+
+						var line []byte
+
+						p := make([]byte, bufSize)
+						var n int
+						var err error
+
+						for {
+							n, err = r.Read(p)
+
+							if n > 0 {
+								line = append(line, p[:n]...)
+							}
+
+							if err != nil {
+								assert.True(t, errors.Is(err, io.EOF))
+								break
+							}
+						}
+
+						if len(line) > 0 {
+							lines = append(lines, string(line))
+							line = nil
+						}
+					}
+
+					require.Equal(t, tc.wantLines, lines)
+					require.Equal(t, tc.wantRdrCount, rdrCount)
+				})
+			}
+		})
+	}
+}
